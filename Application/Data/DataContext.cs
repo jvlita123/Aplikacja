5df8bf49d4062564d10e15020156a44b52d877db
Julita Felczuk <80294@student.pb.edu.pl>
2023-07-26 14:09:34 +0200

    Dodaj pliki projektów.

---
+++ Application/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.Extensions.Options;
+
+namespace Application.Data
+{
+	public class DataContext: DbContext
+	{
+	//	public DbSet<Account> Account { get; set; }
+		public DataContext()
+		{
+		}
+
+		public DataContext(DbContextOptions<DataContext> options)
+			: base(options)
+		{
+		}
+
+	}
+}
